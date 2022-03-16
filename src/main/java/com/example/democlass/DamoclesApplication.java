92ab10b3a6a31eb4a548b17a62a2d8645a97cb4c
sadiarb22 <sadiarasoolbaksh@gmail.com>
2022-03-16 12:07:22 +0500

    first project on spring framework

---
+++ src/main/java/com/example/democlass/DamoclesApplication.java
@@ -0,0 +1,13 @@
+package com.example.democlass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DamoclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamoclesApplication.class, args);
+    }
+
+}
